c2c444088c45222012c0efc139a570151b0fa094
daniqss <danielqueijo14@gmail.com>
2024-11-02 14:12:54 +0100

    feat: add timer

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,67 @@
+use std::time::{Duration, Instant};
 mod prelude;
 mod year2020;
 
-use prelude::DayResult;
-use year2020::day2020_01;
+use prelude::{Day, DayResult};
+use year2020::{day2020_01_p1, day2020_01_p2};
 
-const YEAR_2020: [fn() -> DayResult; 25] = [
-    day2020_01,
-    || (None, None),
-    || (None, None),
-    || (None, None),
-    || (None, None),
-    || (None, None),
-    || (None, None),
-    || (None, None),
-    || (None, None),
-    || (None, None),
-    || (None, None),
-    || (None, None),
-    || (None, None),
-    || (None, None),
-    || (None, None),
-    || (None, None),
-    || (None, None),
-    || (None, None),
-    || (None, None),
-    || (None, None),
-    || (None, None),
-    || (None, None),
-    || (None, None),
-    || (None, None),
-    || (None, None),
+const YEAR_2020: [Option<Day>; 25] = [
+    Some((day2020_01_p1, day2020_01_p2)),
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
 ];
 
 fn main() {
-    for day in YEAR_2020.iter() {
-        match day() {
-            (Some(part1), Some(part2)) => println!("Part 1: {}\nPart 2: {}", part1, part2),
-            _ => {}
+    for (i, day) in YEAR_2020.iter().enumerate() {
+        if let Some(day) = day {
+            match timer(*day) {
+                ((time, Some(part1)), (_, Some(part2))) => {
+                    println!("Day {:02} -> ", i + 1);
+                    println!(
+                        "Time elapsed -> {}μs\tPart 1 -> {}\tPart 2 -> {}\n",
+                        time.as_micros(),
+                        part1,
+                        part2
+                    );
+                }
+                _ => {}
+            }
         }
     }
+}
+
+fn timer<F: FnOnce() -> DayResult>(f: (F, F)) -> ((Duration, DayResult), (Duration, DayResult)) {
+    let mut result = (None, None);
+
+    let start_0 = Instant::now();
+    result.0 = f.0();
+    let elapsed_0 = start_0.elapsed();
+
+    let start_1 = Instant::now();
+    result.1 = f.1();
+    let elapsed_1 = start_1.elapsed();
+
+    return ((elapsed_0, result.0), (elapsed_1, result.1));
 }
