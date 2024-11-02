84226264b27bbd9e12a27c7b92162db6ba8e6512
daniqss <danielqueijo14@gmail.com>
2024-11-02 15:47:58 +0100

    perf: change day 1 puzzle 2 from O(n**3) to O(n**2)

--- src/year2020/mod.rs
+++ src/year2020/mod.rs
@@ -4,7 +4,7 @@ use day2020_01::Day2020_01;
 
 pub fn year_2020() -> [Option<Box<dyn Day>>; 25] {
     [
-        Some(Box::new(Day2020_01)),
+        Some(Box::new(Day2020_01::new())),
         None,
         None,
         None,
