6107b578229b6b74ed2f2f7daef900649109983b
daniqss <danielqueijo14@gmail.com>
2024-11-04 13:37:37 +0100

    feat: add 2020 day 2

--- src/year2020/mod.rs
+++ src/year2020/mod.rs
@@ -1,11 +1,13 @@
 use crate::prelude::Day;
 mod day2020_01;
+mod day2020_02;
 use day2020_01::Day2020_01;
+use day2020_02::Day2020_02;
 
 pub fn year_2020() -> [Option<Box<dyn Day>>; 25] {
     [
         Some(Box::new(Day2020_01::new())),
-        None,
+        Some(Box::new(Day2020_02::new())),
         None,
         None,
         None,
